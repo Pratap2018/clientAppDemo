c5db86523e625c3f2adf2887472f40967ec52eb8
varsha <varshakumari370@gmail.com>
2023-07-03 10:38:38 +0530

    adding code for credential

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
-const path = require('path');
+const path = require("path");
+const nodePolyfill = require("node-polyfill-webpack-plugin");
 module.exports = {
- "mode": "none",
- "entry": "./src/index.js",
- "output": {
-   "path": __dirname + '/dist',
-   "filename": "bundle.js"
- },
-devServer: {
-   contentBase: path.join(__dirname, 'dist')
- }
-}
+  mode: "none",
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
+  plugins: [new nodePolyfill()],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+  },
+};
